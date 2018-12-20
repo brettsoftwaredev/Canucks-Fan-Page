bc5e13ff19f1560239fe968bc938a9ff00038907
Brett Nelson <Hockeybud2011@hotmail.ca>
2018-12-20 00:05:23 -0600

    modified html and modified the service worker js

---
+++ sw.js
@@ -0,0 +1,45 @@
+const canuckCache = 'canuck-cache-2'
+
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(function(cacheNames) {
+            return Promise.all(
+                cacheNames.filter(function(cacheName) {
+                    return cacheName.startsWith('canuck-') && cacheName !== canuckCache
+                }).map(function(cacheName) {
+                    return caches.delete(cacheName);
+                })
+            );
+        })
+    );
+});
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(canuckCache).then(function(cache) {
+            return cache.addAll(
+                [
+                    '/images/alexander-edler-2018-39.jpg',
+                    '/images/arena.jpg',
+                    '/images/brock.jpg',
+                    '/images/gagner.jpg',
+                    '/images/horvat.jpg',
+                    '/images/logo.jpg',
+                    '/js/app.js',
+                    '/js/gulpfile.js',
+                    '/index.html',
+                    '/',
+                    '/css.css'
+                ]
+            );
+        })
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith( 
+        caches.match(event.request).then(function(response) {
+            return response || fetch(event.request)
+        })
+    );
+});
